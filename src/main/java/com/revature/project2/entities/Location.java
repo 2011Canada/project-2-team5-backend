3272543596acea0db2339daef8206bfbaaa474fe
vincewangto <heng.w214@gmail.com>
2021-01-14 17:45:01 -0500

    Join user with his location

--- src/main/java/com/revature/project2/entities/Location.java
+++ src/main/java/com/revature/project2/entities/Location.java
@@ -1,12 +1,17 @@
 package com.revature.project2.entities;
 
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
@@ -18,30 +23,40 @@ public class Location {
 	@Column(name = "location_id")
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int locationId;
-	
+
 	@Column(name = "location_name")
 	private String locationName;
-	
+
 	@Column(name = "description")
 	private String description;
-	
+
 	@Column(name = "picture")
 	private String picture;
-	
+
 	@Column(name = "adjacent_location_id1")
 	private int adjacentLocation1;
-	
+
 	@Column(name = "adjacent_location_id2")
 	private int adjacentLocation2;
 
-	
+	@OneToMany(mappedBy = "currentLocation", fetch = FetchType.LAZY)
+	@JsonBackReference
+	private List<Player> me;
+
+	public List<Player> getMe() {
+		return me;
+	}
+
+	public void setMe(List<Player> me) {
+		this.me = me;
+	}
+
 	public Location() {
 		super();
 	}
 
-
 	public Location(int locationId, String locationName, String description, String picture, int adjacentLocation1,
-			int adjacentLocation2) {
+			int adjacentLocation2, List<Player> me) {
 		super();
 		this.locationId = locationId;
 		this.locationName = locationName;
@@ -49,68 +64,56 @@ public class Location {
 		this.picture = picture;
 		this.adjacentLocation1 = adjacentLocation1;
 		this.adjacentLocation2 = adjacentLocation2;
+		this.me = me;
 	}
-
 
 	public int getLocationId() {
 		return locationId;
 	}
 
-
 	public void setLocationId(int locationId) {
 		this.locationId = locationId;
 	}
-
 
 	public String getLocationName() {
 		return locationName;
 	}
 
-
 	public void setLocationName(String locationName) {
 		this.locationName = locationName;
 	}
-
 
 	public String getDescription() {
 		return description;
 	}
 
-
 	public void setDescription(String description) {
 		this.description = description;
 	}
-
 
 	public String getPicture() {
 		return picture;
 	}
 
-
 	public void setPicture(String picture) {
 		this.picture = picture;
 	}
-
 
 	public int getAdjacentLocation1() {
 		return adjacentLocation1;
 	}
 
-
 	public void setAdjacentLocation1(int adjacentLocation1) {
 		this.adjacentLocation1 = adjacentLocation1;
 	}
-
 
 	public int getAdjacentLocation2() {
 		return adjacentLocation2;
 	}
 
-
 	public void setAdjacentLocation2(int adjacentLocation2) {
 		this.adjacentLocation2 = adjacentLocation2;
 	}
-
 
 	@Override
 	public int hashCode() {
@@ -124,7 +127,6 @@ public class Location {
 		result = prime * result + ((picture == null) ? 0 : picture.hashCode());
 		return result;
 	}
-
 
 	@Override
 	public boolean equals(Object obj) {
@@ -159,13 +161,11 @@ public class Location {
 		return true;
 	}
 
-
 	@Override
 	public String toString() {
 		return "Location [locationId=" + locationId + ", locationName=" + locationName + ", description=" + description
 				+ ", picture=" + picture + ", adjacentLocation1=" + adjacentLocation1 + ", adjacentLocation2="
 				+ adjacentLocation2 + "]";
 	}
-	
-	
+
 }
